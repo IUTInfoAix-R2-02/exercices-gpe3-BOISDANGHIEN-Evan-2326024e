fbc23c588b3126a38e80cd40acc4069550cdd99c
BOISDANGHIEN-Evan-2326024e <keyzaks23@gmail.com>
2024-04-18 11:12:13 +0200

    Exercice 6 quasiment terminé manque juste les boutons

--- src/main/java/fr/amu/iut/exercice6/IHMPendu.java
+++ src/main/java/fr/amu/iut/exercice6/IHMPendu.java
@@ -2,10 +2,12 @@ package fr.amu.iut.exercice6;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -27,35 +29,77 @@ public class IHMPendu extends Application {
     private boolean lettre_selec_bool=false;
     private Label label_vie;
     private String motChoisi;
+    private GridPane gridPane;
+    private Label label_image;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Dico d1=new Dico();
+        this.gridPane = new GridPane();
         this.borderPane = new BorderPane();
         this.textField = new TextField();
         this.label = new Label(motChoisi);
         this.label_vie = new Label("Nombre de vie : 7");
-        borderPane.setBottom(textField);
-        borderPane.setCenter(label);
-        borderPane.setTop(label_vie);
-        borderPane.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, new CornerRadii(0), Insets.EMPTY)));
+        Dico d1=new Dico();
         String motJeu=d1.getMot();
         ArrayList<Character> listeLettreMot= new ArrayList<>();
         listeLettreMot=initialise_liste_mot(listeLettreMot,motJeu);
         ArrayList<Character> listeLettreEssaye= new ArrayList<>();
-        RecupEssaiJoueur();
-        if(lettre_selec_bool){
-            Essai_Joueur_analyse(listeLettreEssaye,listeLettreMot,lettre_selec);
-        }
-        RecupEssaiJoueur();
-
-        // A completer
+        ArrayList<Character> listeLettreEssayeBonneLettre= new ArrayList<>();
+        String nouveauMot=initialise_mot_choisi(motJeu);
+        this.label_image=new Label();
+        this.label=new Label(nouveauMot);
+        gridPane.addRow(0,label_vie);
+        gridPane.addRow(1,label_image);
+        gridPane.setAlignment(Pos.CENTER);
+        borderPane.setBottom(textField);
+        borderPane.setCenter(label);
+        borderPane.setTop(gridPane);
+        borderPane.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, new CornerRadii(0), Insets.EMPTY)));
         this.scene = new Scene(borderPane, 800, 600);
         primaryStage.setScene(scene);
+        ArrayList<Character> finalListeLettreMot = listeLettreMot;
+        if(vie==7){
+            ImageView vie=new ImageView("exercice6/pendu7.png");
+            label_image.setGraphic(vie);
+        }
+        scene.setOnKeyPressed((KeyEvent event) -> {
+            switch (event.getCode()) {
+                case ENTER:
+                    if(vie==0){
+                        label.setText("Perdu... Dommage !");
+                        break;
+                    }
+                    if(listeLettreEssayeBonneLettre.size()==finalListeLettreMot.size()){
+                        label.setText("Bravo tu as gagné !!");
+                        break;
+                    }
+                    if(textField.getText().length()>1){
+                        System.out.println("Trop long mettez juste une lettre");
+                        break;
+                    }
+                    else{
+                        if(vie==0){
+                            label.setText("Tu as perdu.. Dommage !");
+                        }
+                        if(listeLettreEssayeBonneLettre.size()==finalListeLettreMot.size()){
+                            label.setText("Tu a gagné bravo ! Il te restait ce nombre de vie :"+vie);
+                        }
+                        lettre_selec = textField.getText().charAt(0);
+                        initialiste_liste_essai(listeLettreEssaye, lettre_selec);
+                        System.out.println(listeLettreEssaye);
+                        System.out.println(listeLettreEssayeBonneLettre);
+                        System.out.println(finalListeLettreMot);
+                        Essai_Joueur_analyse(listeLettreEssayeBonneLettre, finalListeLettreMot, lettre_selec);
+                        affichage_changement(finalListeLettreMot, listeLettreEssayeBonneLettre);
+                        break;
+                    }
+            }
+        });
         primaryStage.setTitle("Jeu du pendu");
         primaryStage.setWidth(500);
         primaryStage.setHeight(550);
         primaryStage.show();
+
     }
 
     public ArrayList<Character> initialise_liste_mot(ArrayList<Character> listeLettreMot, String motJeu) {
@@ -65,15 +109,59 @@ public class IHMPendu extends Application {
         return listeLettreMot;
     };
 
-    public ArrayList<Character> initialiste_liste_essai(ArrayList<Character> listeLettreEssaye,char tentative){
+    public void initialiste_liste_essai(ArrayList<Character> listeLettreEssaye, char tentative){
         for(int j=0;j<listeLettreEssaye.size();j++) {
             if( Objects.equals(listeLettreEssaye.get(j), tentative)) {
                 listeLettreEssaye.remove(j);
             }
-            listeLettreEssaye.add(tentative);
-
         }
-        return listeLettreEssaye;
+        listeLettreEssaye.add(tentative);
+    }
+
+    public void Essai_Joueur_analyse(ArrayList<Character> listeLettreEssayeBonneLettre,ArrayList<Character> listeLettreMot,char lettre_selec){
+        boolean b1=false;
+        for (Character character : listeLettreMot) {
+            if (Objects.equals(character, lettre_selec)) {
+                listeLettreEssayeBonneLettre.add(lettre_selec);
+                b1 = true;
+            }
+        }
+        if(!b1){
+            vie=vie-1;
+        }
+        if(vie==6){
+            ImageView vie=new ImageView("exercice6/pendu6.png");
+            label_image.setGraphic(vie);
+        }
+        if(vie==5){
+            ImageView vie=new ImageView("exercice6/pendu5.png");
+            label_image.setGraphic(vie);
+        }
+        if(vie==4){
+            ImageView vie=new ImageView("exercice6/pendu4.png");
+            label_image.setGraphic(vie);
+        }
+        if(vie==3){
+            ImageView vie=new ImageView("exercice6/pendu3.png");
+            label_image.setGraphic(vie);
+        }
+        if(vie==2){
+            ImageView vie=new ImageView("exercice6/pendu2.png");
+            label_image.setGraphic(vie);
+        }
+        if(vie==1){
+            ImageView vie=new ImageView("exercice6/pendu1.png");
+            label_image.setGraphic(vie);
+        }
+        if(vie==0){
+            ImageView vie=new ImageView("exercice6/pendu0.png");
+            label_image.setGraphic(vie);
+        }
+        if(listeLettreEssayeBonneLettre.size()==finalListeLettreMot.size()){
+            ImageView vie=new ImageView("exercice6/penduWin.png");
+            label_image.setGraphic(vie);
+        }
+
     }
 
     public void RecupEssaiJoueur(){
@@ -91,35 +179,38 @@ public class IHMPendu extends Application {
         });
     }
 
-    public void initialise_mot_choisi(String motChoisi){
+    public String initialise_mot_choisi(String motChoisi){
         String nouveau_string = "";
         for(int i=0;i<motChoisi.length();i++) {
             nouveau_string=nouveau_string.concat("#");
         }
+        return nouveau_string;
     }
 
     public void affiche_mot_choisi(String motChoisi,char c){
         motChoisi.replace('#',c);
     }
 
-    public void Essai_Joueur_analyse(ArrayList<Character> listeLettreEssaye,ArrayList<Character> listeLettreMot,char lettre_selec){
-        for(int i=0;i<listeLettreEssaye.size();i++) {
-            if(Objects.equals(listeLettreEssaye.get(i), lettre_selec)) {
-                break;
-            }
-            else{
-                listeLettreEssaye.add(lettre_selec);
-                vie=vie-1;
-                this.label_vie.setText("Nombre e vie : "+vie);
-                for(int j=0;j<listeLettreMot.size();j++) {
-                    if(Objects.equals(listeLettreMot.get(j), lettre_selec)) {
 
-                    }
+    public void affichage_changement(ArrayList<Character> listeLettreMot,ArrayList<Character> listeLettreEssayeBonneLettre){
+        boolean test;
+        String s="";
+        for (Character value : listeLettreMot) {
+            test = false;
+            for (Character character : listeLettreEssayeBonneLettre) {
+                if (Objects.equals(value, character) && !test) {
+                    s = s + value;
+                    test = true;
                 }
             }
+            if (!test) {
+                s = s + "#";
+            }
         }
-
+        label.setText(s);
+        label_vie.setText("Nombre de vie :"+vie);
     }
+
 
     public static void main(String[] args) {
         launch(args);

706870c22760ec755937bcf1ccf69d1a460ec8c0
BOISDANGHIEN-Evan-2326024e <keyzaks23@gmail.com>
2024-05-22 08:52:45 +0200

    Exercice 10 terminé

--- src/main/java/fr/amu/iut/exercice10/Conteneurs.java
+++ src/main/java/fr/amu/iut/exercice10/Conteneurs.java
@@ -2,8 +2,14 @@ package fr.amu.iut.exercice10;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -17,9 +23,12 @@ public class Conteneurs extends Application {
     public void start(Stage stage) {
         try {
             BorderPane root = FXMLLoader.load(getClass().getClassLoader().getResource("exercice10/ConteneursView.fxml"));
-            stage.setScene(new Scene(root));
-            stage.setTitle("Premier exemple manipulant les conteneurs");
+            stage.setTitle("Fenetre Logiciel");
+
+            Scene scene1 = new Scene(root, 1200, 800);
+            stage.setScene(scene1);
             stage.show();
+
         } catch (Exception e) {
             e.printStackTrace();
         }

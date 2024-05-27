7c2c89a4bbbfa6966abf85aadcc1b6f377ddc90e
BOISDANGHIEN-Evan-2326024e <keyzaks23@gmail.com>
2024-05-27 11:41:25 +0200

    Exercice 15 terminé

--- src/main/java/fr/amu/iut/exercice15/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice15/LoginControl.java
@@ -1,20 +1,53 @@
-package fr.amu.iut.exercice5;
+package fr.amu.iut.exercice15;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.BooleanBinding;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
-public class LoginControl extends GridPane {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginControl extends GridPane implements Initializable {
     @FXML
     private TextField userId;
 
     @FXML
     private PasswordField pwd;
 
-    private void createBindings() {
-        // MÉTHODE À COMPLÉTER
+    @FXML
+    private Button okBtn;
+
+    @FXML
+    private Button cancelBtn;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Initializing CounterController...");
+        createBindings();
     }
+
+    private void createBindings() {
+        System.out.println("Creating Bindings...");
+        pwd.editableProperty().bind(userId.lengthProperty().greaterThanOrEqualTo(6));
+
+
+        // Binding pour le bouton cancel cliquable ou non
+        cancelBtn.disableProperty().bind(
+                Bindings.createBooleanBinding(() -> userId.getText().isEmpty() && pwd.getText().isEmpty(), userId.textProperty(), pwd.textProperty())
+                );
+
+
+        // Binding pour le bouton ok cliquable ou non
+        BooleanBinding motDePasseValide = Bindings.createBooleanBinding(() ->
+                pwd.getText().length() >= 8 && pwd.getText().matches(".*[A-Z].*") && pwd.getText().matches(".*\\d.*"),pwd.textProperty()
+        );
+        okBtn.disableProperty().bind(motDePasseValide.not());
+    }
+
 
     @FXML
     private void okClicked() {
@@ -29,5 +62,6 @@ public class LoginControl extends GridPane {
     private void cancelClicked() {
         userId.setText("");
         pwd.setText("");
+        System.out.println("lol");
     }
 }
--- src/main/java/fr/amu/iut/exercice15/LoginMain.java
+++ src/main/java/fr/amu/iut/exercice15/LoginMain.java
@@ -1,8 +1,9 @@
-package fr.amu.iut.exercice5;
+package fr.amu.iut.exercice15;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,11 +12,9 @@ public class LoginMain extends Application {
     public static void main(String[] args) {
         Application.launch(args);
     }
-
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("exercice5/LoginView.fxml"));
-
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("exercice15/LoginView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.setTitle("FXML Custom Control");

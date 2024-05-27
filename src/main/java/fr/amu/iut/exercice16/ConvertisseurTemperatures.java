bae90f1931c8793707344b16ac35c85bc42ce20d
BOISDANGHIEN-Evan-2326024e <keyzaks23@gmail.com>
2024-05-27 12:18:24 +0200

    Exercice 16 début

--- src/main/java/fr/amu/iut/exercice16/ConvertisseurTemperatures.java
+++ src/main/java/fr/amu/iut/exercice16/ConvertisseurTemperatures.java
@@ -1,8 +1,12 @@
 package fr.amu.iut.exercice16;
 
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -11,8 +15,18 @@ public class ConvertisseurTemperatures extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        VBox panneauCelsius = new VBox(30);
-        VBox panneauFahrenheit = new VBox(30);
+        Slider slider = new Slider();
+        Slider slider1 = new Slider();
+        slider.setShowTickLabels(true);
+        slider1.setShowTickLabels(true);
+        slider1.setMax(212);
+
+        slider1.valueProperty().bindBidirectional(slider.valueProperty());
+
+
+
+        VBox panneauCelsius = new VBox(30,slider);
+        VBox panneauFahrenheit = new VBox(30,slider1);
 
         HBox root = new HBox(30, panneauCelsius, panneauFahrenheit);
         root.setPadding(new Insets(20));
